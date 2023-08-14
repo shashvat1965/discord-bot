b317b3bc9e6e4f111d7666fa62717c24166d5438
Xcarnage <68941739+shashvat1965@users.noreply.github.com>
2023-08-15 03:32:09 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,13 +6,11 @@ const commands = [];
 // Grab all the command files from the commands directory you created earlier
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
-// Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(token);
 
 // and deploy your commands!
@@ -28,7 +26,6 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
 })();
